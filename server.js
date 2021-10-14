995ff81d3a91d08d8104e4d895a3c3584a2c3e08
nvchien2811 <nvchien2811@gmail.com>
2021-10-14 13:04:15 +0700

    fix-run-server-client

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyparser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 
-const port = process.env.PORT|5000;
+const port = 5000;
 
 var cors = require('cors')
 app.use(cors());
