a7fdf9ac6b9847846b89fe4183ed6e60c3f6b209
trunghopro098 <trungho098@gmail.com>
2021-10-15 23:31:51 +0700

    commit productdetail

--- src/page/App.js
+++ src/page/App.js
@@ -37,7 +37,7 @@ export default function App() {
     getMenu();
     checkUser();
     setshowContent(false); 
-    console.log("abcddd")
+    console.log("abcddde")
   },[])
   const checkUser = async()=>{
     const token = localStorage.getItem("token");
