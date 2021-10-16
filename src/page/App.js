8ce90dcaf44027f42042e8385e787ad8598e885e
nvchien2811 <nvchien2811@gmail.com>
2021-10-16 08:23:47 +0700

    test

--- src/page/App.js
+++ src/page/App.js
@@ -37,7 +37,8 @@ export default function App() {
     getMenu();
     checkUser();
     setshowContent(false); 
-  
+
+    console.log("them cai nz")
   },[])
   const checkUser = async()=>{
     const token = localStorage.getItem("token");
